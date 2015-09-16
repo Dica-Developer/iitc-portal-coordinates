998c5557836273062c6efff72f14ab42a781f4cd
Schaaf, Martin <mschaaf@datameer.com>
2015-09-16 14:36:15 +0200

    remove code that could break IITC functionality

--- iitc-portal-coordiantes.js
+++ iitc-portal-coordiantes.js
@@ -13,15 +13,6 @@ function wrapper(plugin_info) {
     // ensure plugin framework is there, even if iitc is not yet loaded
     if(typeof window.plugin !== 'function') window.plugin = function() {};
 
-    //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
-    //(leaving them in place might break the 'About IITC' page or break update checks)
-    plugin_info.buildName = 'now';
-    plugin_info.dateTimeVersion = '20150627.4522';
-    plugin_info.pluginId = 'save_portals';
-    //END PLUGIN AUTHORS NOTE
-
-
-
     // PLUGIN START ////////////////////////////////////////////////////////
 
     // use own namespace for plugin
